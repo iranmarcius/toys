de9e6de0077eb2b46f83015712c28c644b14a550
Iran <iran@toledo.br>
2012-12-19 09:32:38 -0200

    Melhorias e criação de métodos de conveniência.

--- toys/src/toys/backend/hibernate/SetTransactionIsolationWork.java
+++ toys/src/toys/backend/hibernate/SetTransactionIsolationWork.java
@@ -11,10 +11,16 @@ import org.hibernate.jdbc.Work;
 
 /**
  * Este work seta o nível de isolamento da transação na conexão de uma sessão.
+ * o nível de isolamento anterior é armazenado na propriedade <code>oldLevel</code>.
  * @author Iran
  */
 public class SetTransactionIsolationWork implements Work {
 	private int level;
+	private int oldLevel;
+
+	public SetTransactionIsolationWork() {
+		super();
+	}
 
 	public SetTransactionIsolationWork(int level) {
 		super();
@@ -23,8 +29,31 @@ public class SetTransactionIsolationWork implements Work {
 
 	@Override
 	public void execute(Connection connection) throws SQLException {
-		if (connection.getTransactionIsolation() != level)
+		oldLevel = connection.getTransactionIsolation();
+		if (level != oldLevel)
 			connection.setTransactionIsolation(level);
+	}
+
+	public SetTransactionIsolationWork change(int level) {
+		setLevel(level);
+		return this;
+	}
+
+	public SetTransactionIsolationWork restore() {
+		setLevel(oldLevel);
+		return this;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public int getOldLevel() {
+		return oldLevel;
 	}
 
 }
