071eeaa9a314d8c72644b3f69c0c4a47f5d35c0a
Iran <Iran@CHUCKWALLA.redeinterna.toledo.br>
2016-02-29 16:07:23 -0300

    Alteração no mpétodo de obtenção da sessão de hibernate devido à mudança de versão.

--- toys/src/toys/backend/hibernate/HibernateSession.java
+++ toys/src/toys/backend/hibernate/HibernateSession.java
@@ -1,21 +1,18 @@
 package toys.backend.hibernate;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-
-import toys.fs.FileToys;
 
 /**
  * Classe utilitária para obtenção de sessões do Hibernate.
@@ -34,43 +31,28 @@ public class HibernateSession {
 	 */
 	private static SessionFactory createSessionFactory(SessionFactoryParams params) throws HibernateException {
 		log.debug(String.format("Obtendo sessao (params=%s)", params));
-		Properties props = null;
 		SessionFactory sf = sfm.get(params);
 		if (sf == null) {
-			log.debug("SessionFactory nao encontrado. Un novo sera criado.");
-			Configuration configuration = new Configuration();
+			log.debug("SessionFactory nao encontrado. Um novo sera criado.");
 
-			// Verifica se devem ser carregadas as propriedades de um arquivo especificado.
-			if (StringUtils.isNotBlank(params.getProperties())) {
-				InputStream in = null;
-				try {
-					log.debug(String.format("Utilizando propriedades do arquivo %s", params.getProperties()));
-					in = HibernateSession.class.getClassLoader().getResourceAsStream(params.getProperties());
-					props = new Properties();
-					props.load(in);
-					configuration.setProperties(props);
-				} catch (IOException e) {
-					throw new HibernateException(e);
-				} finally {
-					FileToys.closeQuiet(in);
-				}
+			try {
+				Properties props = new Properties();
+				props.load(HibernateSession.class.getClassLoader().getResourceAsStream(params.getProperties()));
+
+				StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+						.configure(HibernateSession.class.getClassLoader().getResource(params.getMappings()))
+						.applySettings(props)
+						.build();
+
+				sf = new MetadataSources(registry)
+						.buildMetadata()
+						.buildSessionFactory();
+
+				sfm.put(params, sf);
+			} catch (IOException e) {
+				throw new RuntimeException(e);
 			}
 
-			// Verifica se foi especificado algum arquivo de mapeamento.
-			if (!StringUtils.isEmpty(params.getMappings())) {
-				log.debug(String.format("Utilizando mapeamentos do arquivo %s", params.getMappings()));
-				configuration.configure(params.getMappings());
-			} else {
-				log.debug("Utilizando mapeamentos do arquivo padrao");
-				configuration.configure();
-			}
-
-			StandardServiceRegistryBuilder srb = new StandardServiceRegistryBuilder();
-			if (props != null)
-				srb.applySettings(props);
-
-			sf = configuration.buildSessionFactory(srb.build());
-			sfm.put(params, sf);
 		}
 		return sf;
 	}
