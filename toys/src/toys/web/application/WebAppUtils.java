a1ac8bdda04f34b898c5fc54a7b3d9d228b7a042
Iran <Iran@CHUCKWALLA.redeinterna.toledo.br>
2016-10-19 09:16:53 -0200

    Alterações na classe de geração de mensagens de web.
    Classe de geração de mensagens para aplicações de Web foi renomeada para
    WebUtils e o método de geração de mensagens de log foi reformulado.

---
+++ toys/src/toys/web/application/WebAppUtils.java
@@ -0,0 +1,38 @@
+package toys.web.application;
+
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Esta classe é utilizada na criação de mensagens de log para aplicações de
+ * ambiente de web pois registra, juntamente com a mensagem informada, as informações
+ * sobre o usuário e o IP de origem da requisição. Alguns métodos a classe
+ * {@link toys.application.Messages Messages} para criação de mensagens padrão de
+ * aplicação.
+ * @author Iran Marcius
+ */
+public class WebAppUtils {
+
+	/**
+	 * Cria uma mensagem padrão para os logs, com nome de usuário e IP de origem
+	 * obtidos a partir da requisição passada.
+	 * @param request Requisição original
+	 * @param msg Mensagem que será criada
+	 * @param data Dados que serão utilizados na formatação da mensagem através do método {@link String#format(String, Object...)}.
+	 * @return Retorna uma mensagem para registro de logs no seguinte formato:
+	 * <code>username - IP - mensagem</code>.
+	 */
+	public static String logMsg(HttpServletRequest request, String msg, Object... data) {
+		StringBuffer sb = new StringBuffer().append(data == null || data.length == 0 ? msg : String.format(msg, data));
+		Principal p = request.getUserPrincipal();
+		if (p != null)
+			sb.append(", username=").append(p.getName());
+		return sb
+			.append(", ip=").append(request.getRemoteAddr())
+			.append(", uri=").append(request.getRequestURI())
+			.append(", referer=").append(request.getHeader("Referer"))
+			.toString();
+	}
+
+}
