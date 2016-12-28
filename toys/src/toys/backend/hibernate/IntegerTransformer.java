f0b438db1968289bd860b6b5147c4f9c09cb684c
Iran <Iran@CHUCKWALLA.redeinterna.toledo.br>
2016-12-28 09:11:42 -0200

    Depreciação da classe.

--- toys/src/toys/backend/hibernate/IntegerTransformer.java
+++ toys/src/toys/backend/hibernate/IntegerTransformer.java
@@ -6,6 +6,7 @@ import org.hibernate.transform.BasicTransformerAdapter;
  * Converte o primeiro resultado da tupla para inteiro. O primeiro resultado
  * deverá necessariamente inteiro.
  * @author Iran
+ * @deprecated Result transformers cairam em desuso.
  */
 public class IntegerTransformer extends BasicTransformerAdapter {
 	private static final long serialVersionUID = 766622019606439869L;
