0d5fcf02dd4c0b55e77c3eb06a9e9c22bb4ab3e1
Iran <Iran@Chuckwalla.redeinterna.toledo.br>
2017-04-26 17:59:35 -0300

    Renomeação e criação de novas constantes de bancos de dados.

--- toys/src/toys/constants/DBConsts.java
+++ toys/src/toys/constants/DBConsts.java
@@ -9,12 +9,17 @@ public class DBConsts {
 	/**
 	 * Limite mínimo de data e hora para o driver jTDS.
 	 */
-	public static final long MSSQL_TIMESTAMP_MINIMO = -6847793999296l;
+	public static final long JTDS_MINTS = -6847793999296l;
 
 	/**
 	 * Limite máximo de data e hora para o driver jTDS.
 	 */
-	public static final long MSSQL_TIMESTAMP_MAXIMO = 253402307999704l;
+	public static final long JTDS_MAXTS = 253402307999704l;
+
+	/**
+	 * Nome da função GETDATE do SQL Server.
+	 */
+	public static final String MSSQL_GETDATE_FUNC = "getdate";
 
 	private DBConsts() {
 	}
