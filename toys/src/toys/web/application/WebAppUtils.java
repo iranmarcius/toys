0ae717ab6ca081afe52b0adb1f0b0615db1b2192
Iran <Iran@Chuckwalla.redeinterna.toledo.br>
2017-05-31 10:50:52 -0300

    Formatação de código.

--- toys/src/toys/web/application/WebAppUtils.java
+++ toys/src/toys/web/application/WebAppUtils.java
@@ -14,28 +14,28 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class WebAppUtils {
 
-	private WebAppUtils() {
-		super();
-	}
+    private WebAppUtils() {
+        super();
+    }
 
-	/**
-	 * Cria uma mensagem padrão para os logs de aplicações de web.
-	 * @param request Requisição original
-	 * @param msg Mensagem que será criada
-	 * @param detalhes Se for especificado TRUE inclui URI e REFERER na mensagem.
-	 * @param data Dados que serão utilizados na formatação da mensagem através do método {@link String#format(String, Object...)}.
-	 * @return Retorna uma mensagem para registro de logs no seguinte formato:
-	 * <code>username - IP - mensagem</code>.
-	 */
-	public static synchronized String logMsg(HttpServletRequest request, String msg, Object... data) {
-		String s = data == null || data.length == 0 ? msg : String.format(msg, data);
-		Principal p = request.getUserPrincipal();
-		if (p != null)
-			s += ", username=" + p.getName();
-		return s +
-				", ip=" + request.getRemoteAddr() +
-				", uri=" + request.getRequestURI() +
-				", referer=" + request.getHeader("Referer");
-	}
+    /**
+     * Cria uma mensagem padrão para os logs de aplicações de web.
+     * @param request Requisição original
+     * @param msg Mensagem que será criada
+     * @param detalhes Se for especificado TRUE inclui URI e REFERER na mensagem.
+     * @param data Dados que serão utilizados na formatação da mensagem através do método {@link String#format(String, Object...)}.
+     * @return Retorna uma mensagem para registro de logs no seguinte formato:
+     * <code>username - IP - mensagem</code>.
+     */
+    public static synchronized String logMsg(HttpServletRequest request, String msg, Object... data) {
+        String s = data == null || data.length == 0 ? msg : String.format(msg, data);
+        Principal p = request.getUserPrincipal();
+        if (p != null)
+            s += ", username=" + p.getName();
+        return s +
+                ", ip=" + request.getRemoteAddr() +
+                ", uri=" + request.getRequestURI() +
+                ", referer=" + request.getHeader("Referer");
+    }
 
 }
