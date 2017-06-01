d1133f76617a5c7d91cf4e2df422b84a98e9f7ef
Iran <Iran@Chuckwalla.redeinterna.toledo.br>
2017-06-01 13:37:25 -0300

    Utilização do Log4j2.

--- toys/src/toys/application/DeleteTemporaryFilesJob.java
+++ toys/src/toys/application/DeleteTemporaryFilesJob.java
@@ -4,8 +4,8 @@ import java.io.File;
 import java.io.FileFilter;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
@@ -23,36 +23,36 @@ import org.quartz.JobExecutionException;
  * @author Iran
  */
 public class DeleteTemporaryFilesJob implements Job {
-	private static final Log log = LogFactory.getLog(DeleteTemporaryFilesJob.class);
+    private final Logger logger = LogManager.getLogger();
 
-	@Override
-	public void execute(JobExecutionContext context) throws JobExecutionException {
-		JobDataMap data = context.getJobDetail().getJobDataMap();
-		String regex = data.getString("regex");
-		Long maxAge = data.getLong("maxAge");
-		Boolean debug = data.getBoolean("debug");
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        JobDataMap data = context.getJobDetail().getJobDataMap();
+        String regex = data.getString("regex");
+        Long maxAge = data.getLong("maxAge");
+        Boolean debug = data.getBoolean("debug");
 
-		log.info(String.format("Iniciando exclusao de arquivos temporarios. regex=%s, idade maxima=%dms", regex, maxAge));
+        logger.info(String.format("Iniciando exclusao de arquivos temporarios. regex=%s, idade maxima=%dms", regex, maxAge));
 
-		// Filtra os arquivos que se enquadrem no critério de deleção
-		File[] tempFiles = FileUtils.getTempDirectory().listFiles(new FileFilter() {
-			@Override
-			public boolean accept(File f) {
-				return f.isFile() && f.canWrite() && (System.currentTimeMillis() - f.lastModified() > maxAge) && f.getName().matches(regex);
-			}
-		});
+        // Filtra os arquivos que se enquadrem no critério de deleção
+        File[] tempFiles = FileUtils.getTempDirectory().listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File f) {
+                return f.isFile() && f.canWrite() && (System.currentTimeMillis() - f.lastModified() > maxAge) && f.getName().matches(regex);
+            }
+        });
 
-		log.info(String.format("%d arquivos temporarios encontrados.", tempFiles.length));
-		for (File f: tempFiles)
-			if (debug == null || !debug)
-				if (f.delete())
-					log.info(String.format("%s deletado com sucesso.", f.getAbsolutePath()));
-				else
-					log.warn(String.format("Problemas deletando arquivo %s.", f.getAbsolutePath()));
-			else
-				log.info(String.format("Arquivo temporario encontrado: %s", f.getAbsolutePath()));
+        logger.info(String.format("%d arquivos temporarios encontrados.", tempFiles.length));
+        for (File f: tempFiles)
+            if (debug == null || !debug)
+                if (f.delete())
+                    logger.info(String.format("%s deletado com sucesso.", f.getAbsolutePath()));
+                else
+                    logger.warn(String.format("Problemas deletando arquivo %s.", f.getAbsolutePath()));
+            else
+                logger.info(String.format("Arquivo temporario encontrado: %s", f.getAbsolutePath()));
 
-		log.info("Exclusao de arquivos temporarios finalizada.");
-	}
+        logger.info("Exclusao de arquivos temporarios finalizada.");
+    }
 
 }
