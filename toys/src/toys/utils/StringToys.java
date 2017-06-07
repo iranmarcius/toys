7722dfd247e844700c718e5a26ff615c7a10de67
Iran <Iran@Chuckwalla.redeinterna.toledo.br>
2017-06-07 11:52:15 -0300

    Correções menores e remoção de arquivos não mais utilizados.

--- toys/src/toys/utils/StringToys.java
+++ toys/src/toys/utils/StringToys.java
@@ -8,10 +8,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
 import toys.constants.RegExprConsts;
 
@@ -21,267 +19,230 @@ import toys.constants.RegExprConsts;
  */
 public class StringToys {
 
-	/**
-	 * Retorna se uma string está num formato que represente um número com casas
-	 * decimais.
-	 * @param number String representando um número
-	 * @return <code>boolean</code>
-	 */
-	public static boolean isDecimalFormat(String number) {
-		return (number != null) && number.matches(RegExprConsts.DECIMAL_NUMBER);
-	}
+    private StringToys() {
+        super();
+    }
 
-	/**
-	 * Retorna uma string fornecida com seu tamanho limitado para o máximo informado.
-	 * @param s String que será processada
-	 * @param maxLength Tamanho máximo do valor que será retornado
-	 * @return Retorna a string com o tamanho truncado para o máximo permitido ou, caso
-	 * o valor original seja nulo ou seu tamanho não ultrapasse o máximo, a própria
-	 * string.
-	 */
-	public static String limitLength(String s, int maxLength) {
-		return (s == null) || (s.length() <= maxLength) ? s : s.substring(0, maxLength);
-	}
+    /**
+     * Retorna se uma string está num formato que represente um número com casas
+     * decimais.
+     * @param number String representando um número
+     * @return <code>boolean</code>
+     */
+    public static boolean isDecimalFormat(String number) {
+        return (number != null) && number.matches(RegExprConsts.DECIMAL_NUMBER);
+    }
 
-	/**
-	 * Gera uma string com uma quantidade de espaços à direita e limita seu tamanho
-	 * ao limite informado.
-	 * @param s String original.
-	 * @param maxLength Tamanho da string resultante
-	 * @return <code>String</code>
-	 */
-	public static String spacesRight(String s, int maxLength) {
-		String fmtStr = "%-" + maxLength + "s";
-		return String.format(fmtStr, s != null ? s : "").substring(0, maxLength);
-	}
+    /**
+     * Retorna uma string fornecida com seu tamanho limitado para o máximo informado.
+     * @param s String que será processada
+     * @param maxLength Tamanho máximo do valor que será retornado
+     * @return Retorna a string com o tamanho truncado para o máximo permitido ou, caso
+     * o valor original seja nulo ou seu tamanho não ultrapasse o máximo, a própria
+     * string.
+     */
+    public static String limitLength(String s, int maxLength) {
+        return (s == null) || (s.length() <= maxLength) ? s : s.substring(0, maxLength);
+    }
 
-	/**
-	 * Retorna um double formatado com zeros à esquerda, limitando seu tamanho ao máximo
-	 * informado e com o número de casas decimais informadas.
-	 * @param n Número que será formatado
-	 * @param maxLength Tamanho máximo
-	 * @param decimals Casas decimais
-	 * @return
-	 */
-	public static String zerosLeft(double n, int maxLength, int decimals) {
-		String fmtStr = "%0" + (maxLength + 1) + "." + decimals + "f";
-		return String.format(fmtStr, n).replaceAll("[\\.,]", "").substring(0, maxLength);
-	}
+    /**
+     * Gera uma string com uma quantidade de espaços à direita e limita seu tamanho
+     * ao limite informado.
+     * @param s String original.
+     * @param maxLength Tamanho da string resultante
+     * @return <code>String</code>
+     */
+    public static String spacesRight(String s, int maxLength) {
+        String fmtStr = "%-" + maxLength + "s";
+        return String.format(fmtStr, s != null ? s : "").substring(0, maxLength);
+    }
 
-	/**
-	 * Retorna um inteiro formatado com zeros à esquerda limitando seu tamanho ao máximo
-	 * informado.
-	 * @param n Número que será formatado
-	 * @param maxLength Tamanho máximo
-	 * @return
-	 */
-	public static String zerosLeft(int n, int maxLength) {
-		String fmtStr = "%0" + maxLength + "d";
-		return String.format(fmtStr, n).substring(0, maxLength);
-	}
+    /**
+     * Retorna um double formatado com zeros à esquerda, limitando seu tamanho ao máximo
+     * informado e com o número de casas decimais informadas.
+     * @param n Número que será formatado
+     * @param maxLength Tamanho máximo
+     * @param decimals Casas decimais
+     * @return
+     */
+    public static String zerosLeft(double n, int maxLength, int decimals) {
+        String fmtStr = "%0" + (maxLength + 1) + "." + decimals + "f";
+        return String.format(fmtStr, n).replaceAll("[\\.,]", "").substring(0, maxLength);
+    }
 
-	/**
-	 * Retorna a representação de uma string em hexadecimal.
-	 * @param s String que será transformada
-	 * @return <code>String</code>
-	 */
-	public static String str2hex(String s) {
-		return str2hex(s.getBytes());
-	}
+    /**
+     * Retorna um inteiro formatado com zeros à esquerda limitando seu tamanho ao máximo
+     * informado.
+     * @param n Número que será formatado
+     * @param maxLength Tamanho máximo
+     * @return
+     */
+    public static String zerosLeft(int n, int maxLength) {
+        String fmtStr = "%0" + maxLength + "d";
+        return String.format(fmtStr, n).substring(0, maxLength);
+    }
 
-	/**
-	 * Retorna o array de bytes como uma string de valores hexadecimais.
-	 * @param b Array de bytes
-	 * @return <code>String</code>
-	 */
-	public static String str2hex(byte[] b) {
-		StringBuffer s = new StringBuffer();
-		for (int i = 0; i < b.length; i++)
-			s.append(String.format("%02x", b[i]));
-		return s.toString();
-	}
+    /**
+     * Retorna a representação de uma string em hexadecimal.
+     * @param s String que será transformada
+     * @return <code>String</code>
+     */
+    public static String str2hex(String s) {
+        return str2hex(s.getBytes());
+    }
 
-	/**
-	 * Converte uma string com representações hexadecimais em uma string normal.
-	 * @param s String com a representação hexadecimal dos bytes
-	 * @return <code>String</code>
-	 */
-	public static String hex2str(String s) {
-		if (s.length() % 2 > 0)
-			return null;
-		byte[] b = new byte[s.length() / 2];
-		for (int i = 0; i < s.length(); i += 2)
-			b[(i + 1) / 2] = (byte)Integer.parseInt(s.substring(i, i + 2), 16);
-		return new String(b);
-	}
+    /**
+     * Retorna o array de bytes como uma string de valores hexadecimais.
+     * @param b Array de bytes
+     * @return <code>String</code>
+     */
+    public static String str2hex(byte[] b) {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < b.length; i++)
+            s.append(String.format("%02x", b[i]));
+        return s.toString();
+    }
 
-	/**
-	 * Retorna um array de inteiros a partir de uma string que representando um número.
-	 * @param s String a ser processada
-	 * @return Returna um array de <code>int</code> com os números contidos na string.
-	 * Caso qualquer um dos caracteres da string não sejam a representação de um
-	 * número, a função retornará <code>null</code>.
-	 */
-	public static int[] toIntArray(String s) {
-		try {
-			int[] ints = new int[s.length()];
-			for (int i = 0; i < s.length(); i++)
-				ints[i] = Integer.parseInt(s.substring(i, i + 1));
-			return ints;
-		} catch (Exception e) {
-			return null;
-		}
-	}
+    /**
+     * Converte uma string com representações hexadecimais em uma string normal.
+     * @param s String com a representação hexadecimal dos bytes
+     * @return <code>String</code>
+     */
+    public static String hex2str(String s) {
+        if (s.length() % 2 > 0)
+            return null;
+        byte[] b = new byte[s.length() / 2];
+        for (int i = 0; i < s.length(); i += 2)
+            b[(i + 1) / 2] = (byte)Integer.parseInt(s.substring(i, i + 2), 16);
+        return new String(b);
+    }
 
-	/**
-	 * Converte um valor string numa lista de valores considerando a vírgula e os finais de linha como separadores de itens.
-	 * @param str Valor string.
-	 * @return <code>List&lt;String&gt;</code>
-	 */
-	public static List<String> toList(String str) {
-		List<String> l = new ArrayList<>();
-		String[] sa = str.split("\r\n");
-		for (String s: sa) {
-			String[] ss = s.split(" *, *");
-			for (String sss: ss)
-				l.add(sss);
-		}
-		return l;
-	}
+    /**
+     * Retorna um array de inteiros a partir de uma string que representando um número.
+     * @param s String a ser processada
+     * @return Returna um array de <code>int</code> com os números contidos na string.
+     * Caso qualquer um dos caracteres da string não sejam a representação de um
+     * número, a função retornará <code>null</code>.
+     */
+    public static int[] toIntArray(String s) {
+        int[] ints = new int[s.length()];
+        for (int i = 0; i < s.length(); i++)
+            ints[i] = Integer.parseInt(s.substring(i, i + 1));
+        return ints;
+    }
 
-	/**
-	 * Retorna uma string que está delimitada entre outras duas informadas ou
-	 * <code>null</code> caso não haja uma string entre as duas informadas.
-	 * @param s String a ser pesquisada
-	 * @param s1 Primeira string
-	 * @param s2 Segunda string
-	 * @return <code>String</code>
-	 */
-	public static String stringBetween(String s, String s1, String s2) {
-		int i = s.indexOf(s1);
-		int j = s.indexOf(s2);
-		if ((i == -1) || (j == -1) || (i > j)) {
-			return null;
-		}
-		return s.substring(i + s1.length(), j);
-	}
+    /**
+     * Converte um valor string numa lista de valores considerando a vírgula e os finais de linha como separadores de itens.
+     * @param str Valor string.
+     * @return <code>List&lt;String&gt;</code>
+     */
+    public static List<String> toList(String str) {
+        List<String> l = new ArrayList<>();
+        String[] sa = str.split("\r\n");
+        for (String s: sa) {
+            String[] ss = s.split(" *, *");
+            for (String sss: ss)
+                l.add(sss);
+        }
+        return l;
+    }
 
-	/**
-	 * Faz substituições das variáveis existentes na string pelos valores
-	 * existentes no objeto de propriedades informado. Serão consideradas variáveis
-	 * todas as expressões escritas no formado <b>${nome}</b>.
-	 * @param s String original.
-	 * @param p mapa de propreidades cujos valores serão utilizados na substituição.
-	 * Os valores que não forem encontrados no objeto de propriedades serão substituídos
-	 * por vazios.
-	 * @return <code>String</code>
-	 */
-	public static String replaceProperties(String s, Properties p) {
+    /**
+     * Retorna uma string que está delimitada entre outras duas informadas ou
+     * <code>null</code> caso não haja uma string entre as duas informadas.
+     * @param s String a ser pesquisada
+     * @param s1 Primeira string
+     * @param s2 Segunda string
+     * @return <code>String</code>
+     */
+    public static String stringBetween(String s, String s1, String s2) {
+        int i = s.indexOf(s1);
+        int j = s.indexOf(s2);
+        if ((i == -1) || (j == -1) || (i > j)) {
+            return null;
+        }
+        return s.substring(i + s1.length(), j);
+    }
 
-		// substitui as propriedades existentes
-		Iterator<Map.Entry<Object, Object>> i = p.entrySet().iterator();
-		while (i.hasNext()) {
-			Map.Entry<Object, Object> entry = i.next();
-			s = s.replaceAll("\\$\\{" + entry.getKey().toString() + "\\}",
-				entry.getValue().toString());
-		}
+    /**
+     * Retorna uma nova string composta pela repetição da string original.
+     * @param s String a ser repetida
+     * @param count Número de repetições
+     * @return String
+     */
+    public static String repeat(String s, int count) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
 
-		// substitui todas as outras variáveis não encontradas no mapa de propriedades
-		// por valores vazios
-		s = s.replaceAll("\\$\\{.+\\}", "");
+    /**
+     * Retorna uma nova string criada a partir da string informada, preenchida com espaços à
+     * direita de acordo com o tamanho especificado.
+     * @param s String original
+     * @param length Tamanho
+     * @return <code>String</code>
+     */
+    public static String forceLength(String s, int length) {
+        String format = new StringBuffer().append("%-").append(length).append("s").toString();
+        return String.format(format, s).substring(0, length);
+    }
 
-		return s;
-	}
+    /**
+     * Processa uma string mantendo apenas os desejados.
+     * @param s String a ser processada
+     * @param allow Regular expression com os caracteres que devem ser mantidos na string.
+     * @return String
+     */
+    public static String cleanValue(String s, String allow) {
+        if (s == null)
+            return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++)
+            if (s.substring(i, i + 1).matches(allow))
+                sb.append(s.charAt(i));
+        return sb.toString();
+    }
 
-	/**
-	 * Faz substituições das variáveis existentes na string pelos valores existentes
-	 * nas propriedades do sistema.
-	 * @param s String original
-	 * @see #replaceProperties(String, Properties)
-	 */
-	public static String replaceProperties(String s) {
-		return replaceProperties(s, System.getProperties());
-	}
+    /**
+     * Retorna uma string criada a partir do erro informado.
+     * @param t Objeto com o erro
+     * @return <code>String</code>
+     */
+    public static String toString(Throwable t) {
+        ByteArrayOutputStream bao = new ByteArrayOutputStream();
+        t.printStackTrace(new PrintStream(bao));
+        return new String(bao.toByteArray());
+    }
 
-	/**
-	 * Retorna uma nova string composta pela repetição da string original.
-	 * @param s String a ser repetida
-	 * @param count Número de repetições
-	 * @return String
-	 */
-	public static String repeat(String s, int count) {
-		StringBuffer sb = new StringBuffer();
-		for (int i = 0; i < count; i++) {
-			sb.append(s);
-		}
-		return sb.toString();
-	}
+    /**
+     * Retorna a primeira palavra antes do primeiro espaço em uma string.
+     * Caso não haja espaços retorna a string original.
+     * @param s String
+     * @return String
+     */
+    public static String firstWord(String s) {
+        if (s == null)
+            return null;
+        int i = s.indexOf(' ');
+        return i > -1 ? s.substring(0, i) : s;
+    }
 
-	/**
-	 * Retorna uma nova string criada a partir da string informada, preenchida com espaços à
-	 * direita de acordo com o tamanho especificado.
-	 * @param s String original
-	 * @param length Tamanho
-	 * @return <code>String</code>
-	 */
-	public static String forceLength(String s, int length) {
-		String format = new StringBuffer().append("%-").append(length).append("s").toString();
-		return String.format(format, s).substring(0, length);
-	}
-
-	/**
-	 * Processa uma string mantendo apenas os desejados.
-	 * @param s String a ser processada
-	 * @param allow Regular expression com os caracteres que devem ser mantidos na string.
-	 * @return String
-	 */
-	public static String cleanValue(String s, String allow) {
-		if (s == null)
-			return null;
-		StringBuffer sb = new StringBuffer();
-		for (int i = 0; i < s.length(); i++)
-			if (s.substring(i, i + 1).matches(allow))
-				sb.append(s.charAt(i));
-		return sb.toString();
-	}
-
-	/**
-	 * Retorna uma string criada a partir do erro informado.
-	 * @param t Objeto com o erro
-	 * @return <code>String</code>
-	 */
-	public static String toString(Throwable t) {
-		ByteArrayOutputStream bao = new ByteArrayOutputStream();
-		t.printStackTrace(new PrintStream(bao));
-		return new String(bao.toByteArray());
-	}
-
-	/**
-	 * Retorna a primeira palavra antes do primeiro espaço em uma string.
-	 * Caso não haja espaços retorna a string original.
-	 * @param s String
-	 * @return String
-	 */
-	public static String firstWord(String s) {
-		if (s == null)
-			return null;
-		int i = s.indexOf(" ");
-		return i > -1 ? s.substring(0, i) : s;
-	}
-
-	/**
-	 * Retorna um mapa de chaves e valores a partir de uma string com um formato específico.
-	 * @param s String com os nomes de chaves e valores. Deve estar no formado <code>chave1=valor1|chave2=valor2|...chaveN=valorN</code>.
-	 * @return {@link Map}
-	 */
-	public static Map<String, String> toMap(String s) {
-		String[] kvs = s.split("\\|");
-		Map<String, String> m = new HashMap<>();
-		for (String kv: kvs) {
-			String[] ss = kv.split("\\=");
-			m.put(ss[0], ss[1]);
-		}
-		return m;
-	}
+    /**
+     * Retorna um mapa de chaves e valores a partir de uma string com um formato específico.
+     * @param s String com os nomes de chaves e valores. Deve estar no formado <code>chave1=valor1|chave2=valor2|...chaveN=valorN</code>.
+     * @return {@link Map}
+     */
+    public static Map<String, String> toMap(String s) {
+        String[] kvs = s.split("\\|");
+        Map<String, String> m = new HashMap<>();
+        for (String kv: kvs) {
+            String[] ss = kv.split("\\=");
+            m.put(ss[0], ss[1]);
+        }
+        return m;
+    }
 
 }
--- toys/test-src/toys/tests/junit/StringToysTestCase.java
+++ toys/test-src/toys/tests/junit/StringToysTestCase.java
@@ -1,7 +1,5 @@
 package toys.tests.junit;
 
-import java.util.Properties;
-
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -9,24 +7,15 @@ import toys.utils.StringToys;
 
 public class StringToysTestCase extends TestCase {
 
-	public static Test suite() {
-		return new TestSuite(StringToysTestCase.class);
-	}
+    public static Test suite() {
+        return new TestSuite(StringToysTestCase.class);
+    }
 
-	public void testReplacePropertiesStringProperties() {
-		String s= "${name} - ${name} - ${year}";
-		Properties p = new Properties();
-		p.put("name", "Garibaldi");
-		p.put("year", "1972");
-		s = StringToys.replaceProperties(s, p);
-		assertEquals("Garibaldi - Garibaldi - 1972", s);
-	}
-
-	public void testSpacesRight() {
-		assertEquals(StringToys.spacesRight("teste", 10), "teste     ");
-		assertEquals(StringToys.spacesRight("teste", 5), "teste");
-		assertEquals(StringToys.zerosLeft(2.3, 5, 2), "00230");
-		assertEquals(StringToys.zerosLeft(2, 5), "00002");
-	}
+    public void testSpacesRight() {
+        assertEquals(StringToys.spacesRight("teste1", 10), "teste1     ");
+        assertEquals(StringToys.spacesRight("teste2", 5), "teste2");
+        assertEquals(StringToys.zerosLeft(2.3, 5, 2), "00230");
+        assertEquals(StringToys.zerosLeft(2, 5), "00002");
+    }
 
 }
