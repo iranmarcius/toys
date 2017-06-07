507e2f3dfe0e143ac2e129ab76726442fe053c5c
Iran <Iran@Chuckwalla.redeinterna.toledo.br>
2017-06-07 11:54:59 -0300

    Remoção de arquivos sem utilidade.

--- toys/build/cvsmail.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-my ($user, $verInfo, $to) = @ARGV;
-
-open(F, "| /bin/mail -s \"[cvs-commit] $user - $verInfo\"  $to");
-while (defined($l = <STDIN>)) {
-	print F $l;
-}
-close(F);
