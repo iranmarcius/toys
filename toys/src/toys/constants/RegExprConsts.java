2a2b4739d3820c4f5f7ede46bdc081d215d2bead
Iran <Iran@Chuckwalla.redeinterna.toledo.br>
2017-06-13 11:17:09 -0300

    Alterações nas validações.
    Alterada a regular expression para validação de e-mails e criado um novo
    método para validação desses.

--- toys/src/toys/constants/RegExprConsts.java
+++ toys/src/toys/constants/RegExprConsts.java
@@ -5,84 +5,84 @@ package toys.constants;
  */
 public class RegExprConsts {
 
-	/**
-	 * Formato de e-mail.
-	 */
-	public static final String EMAIL = "^[a-zA-Z0-9_]([\\.\\-_]?[a-zA-Z0-9_\\-]+)*@[a-zA-Z0-9]+(\\.[a-zA-Z0-9]+)+";
+    /**
+     * Formato de e-mail.
+     */
+    public static final String EMAIL = "^[a-zA-Z0-9_][a-zA-Z0-9_\\.\\-]*[a-zA-Z0-9_]@([a-zA-Z0-9_\\-]+\\.)+[a-zA-Z]+$";
 
-	/**
-	 * Formato para validação de números com separador decimal.
-	 * O número não deverá conter separadores de milhar e o separador decimal poderá
-	 * o ponto ou a vírgula.
-	 */
-	public static final String DECIMAL_NUMBER = "^\\d+([,\\.]\\d+)?$";
+    /**
+     * Formato para validação de números com separador decimal.
+     * O número não deverá conter separadores de milhar e o separador decimal poderá
+     * o ponto ou a vírgula.
+     */
+    public static final String DECIMAL_NUMBER = "^\\d+([,\\.]\\d+)?$";
 
-	/**
-	 * Formato de moeda brasileira.
-	 */
-	public static final String CURRENCY_BR = "^(\\d{1,3}\\.)*\\d{1,3},\\d+$";
+    /**
+     * Formato de moeda brasileira.
+     */
+    public static final String CURRENCY_BR = "^(\\d{1,3}\\.)*\\d{1,3},\\d+$";
 
-	/**
-	 * Formato de CEP.
-	 */
-	public static final String ZIP = "^\\d{5,5}\\-?\\d{3,3}$";
+    /**
+     * Formato de CEP.
+     */
+    public static final String ZIP = "^\\d{5,5}\\-?\\d{3,3}$";
 
    /**
     * Formato para hora, com segundos e milissegundos opcionais.
     */
-	public static final String TIME = "^\\d{1,3}(:\\d{1,2}){1,2}(\\.\\d{1,3})?$";
+    public static final String TIME = "^\\d{1,3}(:\\d{1,2}){1,2}(\\.\\d{1,3})?$";
 
    /**
     * Formato para hora com milissegundos opcionais.
     */
-	public static final String TIME_SEC = "^\\d{1,3}(:\\d{1,2}){2,2}(\\.\\d{1,3})?$";
+    public static final String TIME_SEC = "^\\d{1,3}(:\\d{1,2}){2,2}(\\.\\d{1,3})?$";
 
    /**
     * Formato de hora completo.
     */
-	public static final String TIME_MS = "^\\d{1,3}(:\\d{1,2}){2,2}\\.\\d{1,3}$";
+    public static final String TIME_MS = "^\\d{1,3}(:\\d{1,2}){2,2}\\.\\d{1,3}$";
 
    /**
     * Formato simples de hora.
     */
-	public static final String SIMPLE_TIME = "^\\d{1,2}(:\\d{1,2}){0,2}$";
+    public static final String SIMPLE_TIME = "^\\d{1,2}(:\\d{1,2}){0,2}$";
 
    /**
     * Formato de IPv4
     */
-	public static final String IPV4 = "^(\\d{1,3}\\.){3,3}\\d{1,3}$";
+    public static final String IPV4 = "^(\\d{1,3}\\.){3,3}\\d{1,3}$";
 
-	/*
-	 * Formatos localizados
-	 */
+    /*
+     * Formatos localizados para o Brasil
+     */
 
-	/**
-	 * Formato de data localizado para o Brasil.
-	 */
-	public static final String DATE_BR = "^\\d{1,2}/\\d{1,2}/\\d{4}$";
+    /**
+     * Formato de data localizado para o Brasil.
+     */
+    public static final String DATE_BR = "^\\d{1,2}/\\d{1,2}/\\d{4}$";
 
-	/**
-	 * Formato de data SQL
-	 */
-	public static final String DATE_SQL = "^\\d{4,4}\\-\\d{2,2}\\-\\d{2,2}$";
+    /**
+     * Formato de data SQL
+     */
+    public static final String DATE_SQL = "^\\d{4,4}\\-\\d{2,2}\\-\\d{2,2}$";
 
-	/**
-	 * Formato de timestamp
-	 */
-	public static final String TIMESTAMP = "^\\d{4,4}\\-\\d{2,2}\\-\\d{2,2} \\d{2,2}:\\d{2,2}:{2,2}\\.\\d{3,3}$";
+    /**
+     * Formato de timestamp
+     */
+    public static final String TIMESTAMP = "^\\d{4,4}\\-\\d{2,2}\\-\\d{2,2} \\d{2,2}:\\d{2,2}:{2,2}\\.\\d{3,3}$";
 
-	/**
-	 * CPF
-	 */
-	public static final String CPF = "^\\d{3,3}\\.\\d{3,3}\\.\\d{3,3}[-/]\\d{2,2}$";
+    /**
+     * CPF
+     */
+    public static final String CPF = "^\\d{3,3}\\.\\d{3,3}\\.\\d{3,3}[-/]\\d{2,2}$";
 
-	/**
-	 * CNPJ
-	 */
+    /**
+     * CNPJ
+     */
+    public static final String CNPJ = "^\\d{2}(\\.?\\d{3}){2}/\\d{4}\\-?\\d{2}$";
 
-	public static final String CNPJ = "^\\d{2}(\\.?\\d{3}){2}/\\d{4}\\-?\\d{2}$";
-
-	private RegExprConsts() {
-	}
+    private RegExprConsts() {
+        super();
+    }
 
 }
