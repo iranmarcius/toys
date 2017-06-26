c2a38c1e32f84869eacd4b10952dede17e6db50f
Iran <Iran@Chuckwalla.redeinterna.toledo.br>
2017-06-26 12:26:37 -0300

    Atualização dos métodos de JNDI.

--- toys/src/toys/utils/JNDIUtils.java
+++ toys/src/toys/utils/JNDIUtils.java
@@ -7,69 +7,58 @@ import javax.naming.NamingException;
 import javax.rmi.PortableRemoteObject;
 
 /**
- * Métodos utilitários e comuns para interação com JNDI.
+ * Métodos utilitários para operaçoes JNDI.
  * @author Iran Marcius
  */
-public class JNDIUtils {
-	private static Context ic;
+public final class JNDIUtils {
+    private static Context ic;
 
-	/**
-	 * Nome default da sessão de e-mail.
-	 */
-	public static final String DEFAULT_MAIL_SESSION_PATH = "java:comp/env/mail/Session";
+    /**
+     * Nome default da sessão de e-mail.
+     */
+    public static final String DEFAULT_MAIL_SESSION_PATH = "java:comp/env/mail/Session";
 
-	/**
-	 * Retorna um contexto inicial.
-	 * O contexto será criado e cacheado na primeira chamada. Nas chamadas subseqüentes será
-	 * retornado o contexto armazenado em cache.
-	 * @return Context
-	 * @throws NamingException
-	 */
-	public static Context getInitialContext() throws NamingException {
-		if (ic == null)
-			ic = new InitialContext();
-		return ic;
-	}
+    protected JNDIUtils() {
+        super();
+    }
 
-	/**
-	 * Retorna uma mail hs com o nome default de sessão de e-mail.
-	 * @return Session
-	 * @see #DEFAULT_MAIL_SESSION_PATH
-	 * @throws NamingException
-	 */
-	public static Session getMailSession() throws NamingException {
-		return getMailSession(DEFAULT_MAIL_SESSION_PATH);
-	}
+    /**
+     * Retorna uma instância contexto inicial JNDI.
+     */
+    public synchronized static Context getInitialContext() throws NamingException {
+        if (ic == null)
+            ic = new InitialContext();
+        return ic;
+    }
 
-	/**
-	 * Retorna a sessão de e-mail correspondente ao caminho fornecido.
-	 * Caso nenhum nome tenha sido especificado, utiliza o nome detault.
-	 * @param jndiPath
-	 * @return Session
-	 * @throws NamingException
-	 * @see #DEFAULT_MAIL_SESSION_PATH
-	 */
-	public static Session getMailSession(String jndiPath) throws NamingException {
-		if (jndiPath == null) {
-			jndiPath = DEFAULT_MAIL_SESSION_PATH;
-		}
-		Object o = getInitialContext().lookup(jndiPath);
-		return (Session)PortableRemoteObject.narrow(o, Session.class);
-	}
+    /**
+     * Retorna uma sessão de email com o nome default de sessão de e-mail.
+     * @return Session
+     * @see #DEFAULT_MAIL_SESSION_PATH
+     * @throws NamingException
+     */
+    public synchronized static Session getMailSession() throws NamingException {
+        return getMailSession(DEFAULT_MAIL_SESSION_PATH);
+    }
 
-	/**
-	 * Procura por um objeto do tipo <code>java.lang.Boolean</code> no contexto com o nome
-	 * <b>debugging</b> (<code>java:comp/env/debugging</code>). Caso não encontre o objeto,
-	 * retorna o valor <code>FALSE</code>.
-	 * @return <code>boolean</code>
-	 * @throws NamingException
-	 */
-	public static boolean isDebugging() throws NamingException {
-		Object o = getInitialContext().lookup("java:comp/env/debug");
-		if ((o != null) && (o instanceof Boolean))
-			return ((Boolean)o).booleanValue();
-		else
-			return false;
-	}
+    /**
+     * Retorna a sessão de e-mail correspondente ao caminho fornecido. Caso nenhum nome tenha sido especificado, utiliza o nome detault.
+     * @param jndiPath
+     * @return Session
+     * @throws NamingException
+     * @see #DEFAULT_MAIL_SESSION_PATH
+     */
+    public synchronized static Session getMailSession(String jndiPath) throws NamingException {
+        Object o = getInitialContext().lookup(jndiPath != null ? jndiPath : DEFAULT_MAIL_SESSION_PATH);
+        return (Session)PortableRemoteObject.narrow(o, Session.class);
+    }
+
+    /**
+     * Retorna o valor da propriedade <b>ambienteDesenvolvimento</b> armazenada no contexto JNDI.
+     * @throws NamingException
+     */
+    public synchronized static boolean isAmbienteDesenvolvimento() throws NamingException {
+        return (Boolean)getInitialContext().lookup("java:comp/env/ambienteDesenvolvimento");
+    }
 
 }
