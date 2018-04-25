7ade1d38117283ea603ffc4f9b65cd3db438bd04
Iran Marcius <iran@toledo.br>
2018-04-25 07:20:54 -0300

    Melhoria no código dos testes.

--- src/test/java/toys/tests/junit/CollectionToysTest.java
+++ src/test/java/toys/tests/junit/CollectionToysTest.java
@@ -1,36 +1,35 @@
 package toys.tests.junit;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import toys.CollectionToys;
 
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Before;
-import org.junit.Test;
-
-import toys.CollectionToys;
+import static org.junit.Assert.assertEquals;
 
 public class CollectionToysTest {
     private List<Integer> lista;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         lista = Arrays.asList(0, 1, 2, 3, 4, 5);
     }
 
     @Test
     public void testFragmentar() {
         List<List<Integer>> listas = CollectionToys.fragmentar(lista, 3);
-        assertEquals(listas.size(), 2);
-        assertEquals(listas.get(0).size(), 3);
-        assertEquals(listas.get(1).size(), 3);
+        assertEquals(2, listas.size());
+        assertEquals(3, listas.get(0).size());
+        assertEquals(3, listas.get(1).size());
     }
 
     @Test
     public void testToStringList() {
-        assertEquals(CollectionToys.asString(lista), "0, 1, 2, 3, 4, 5");
-        assertEquals(CollectionToys.asString(lista, "-"), "0-1-2-3-4-5");
-        assertEquals(CollectionToys.asString(lista, "-", "'"), "'0'-'1'-'2'-'3'-'4'-'5'");
+        assertEquals("0, 1, 2, 3, 4, 5", CollectionToys.asString(lista));
+        assertEquals("0-1-2-3-4-5", CollectionToys.asString(lista, "-"));
+        assertEquals("'0'-'1'-'2'-'3'-'4'-'5'", CollectionToys.asString(lista, "-", "'"));
     }
 
 }
