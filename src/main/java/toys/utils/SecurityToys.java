05b5e91fbcb69d1aa21070ec72d60f516f32902a
Iran Marcius <iran@toledo.br>
2018-12-04 14:16:58 -0200

    Melhoria nos métodos para obtenção de tokens e documentação.

--- src/main/java/toys/utils/SecurityToys.java
+++ src/main/java/toys/utils/SecurityToys.java
@@ -8,37 +8,65 @@ import java.security.Key;
 
 /**
  * Métodos utilitários para implementações de segurança.
+ *
  * @author Iran
  * @since 19/11/2018
  */
 public class SecurityToys {
 
+    private SecurityToys() {
+        super();
+    }
+
     /**
-     * Retorna o token JWT armazenado no cabeçalho <b>Authorization</b> da requisição. O esquema utilizado deverá ser o <b>Bearer</b>, do contrário
-     * a informação do cabeçalho será ignorada.
-     * @param request Objeto contendo as informações da requisição.
-     * @return Retorna o token encontraro ou nulo.
+     * Extrai o token JWT do cabeçalho informado. O método retorna o valor logo após a palavra <b>Bearer</b>.
+     * Caso a palavra não seja encontrada será retornado um valor nulo.
+     *
+     * @param header Valor do cabeçalho.
+     * @return Retorna uma string representando o token JWT ou nulo caso não haja um token.
      */
-    public static String getJWT(HttpServletRequest request) {
-        String authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer"))
-            return authorizationHeader.substring(7);
+    public static String getJWTFromHeader(String header) {
+        if (header != null && header.startsWith("Bearer"))
+            return header.substring(7);
         else
             return null;
     }
 
     /**
-     * Extrai as claims de um token JWT utilizando o método {@link #getJWT(HttpServletRequest)}.
+     * Método de conveniência para invocar o {@link #getJWTFromHeader(String)} passando o valor obtido do cabeçalho
+     * <b>Authorization</b>.
+     *
      * @param request Objeto contendo as informações da requisição.
-     * @param key Chave utilizada para assinatura do token.
-     * @return Caso um token seja encontrado retorna as claims, do contrário retorna nulo.
+     * @return Retorna o token encontrado ou nulo.
      */
-    public static Claims getClaims(HttpServletRequest request, Key key) {
-        String jwt = getJWT(request);
-        if (jwt != null)
-            return Jwts.parser().setSigningKey(key).parseClaimsJws(jwt).getBody();
+    public static String getJWT(HttpServletRequest request) {
+        return getJWTFromHeader(request.getHeader("Authorization"));
+    }
+
+    /**
+     * Decodifica e retorna as claims contidas no token informado.
+     *
+     * @param token Token JWT a ser processado.
+     * @param key   Chave utilizada na decodificação do token.
+     * @return Retorna uma relação de claims ou nulo.
+     */
+    public static Claims getClaims(String token, Key key) {
+        if (token != null)
+            return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
         else
             return null;
+    }
+
+    /**
+     * Método de conveniência para invocar o {@link #getClaims(String, Key)} extraindo o token da requisição
+     * utilizando o método {@link #getJWT(HttpServletRequest)}.
+     *
+     * @param request Objeto contendo as informações da requisição.
+     * @param key     Chave utilizada para assinatura do token.
+     * @return Retorna as claims do token ou nulo.
+     */
+    public static Claims getClaims(HttpServletRequest request, Key key) {
+        return getClaims(getJWT(request), key);
     }
 
 }
