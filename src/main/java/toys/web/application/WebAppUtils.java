7d919f5b664ab1e85afb50a94f6fb64cce66d200
Iran Marcius <iran@toledo.br>
2018-12-04 14:17:27 -0200

    Criação de método utilitário para gerar string com detalhes da requisição.

--- src/main/java/toys/web/application/WebAppUtils.java
+++ src/main/java/toys/web/application/WebAppUtils.java
@@ -16,6 +16,38 @@ public class WebAppUtils {
     }
 
     /**
+     * Retorna uma string com os detalhes da requisição. A string irá conter o IP remoto, o usuário autenticado (caso exista),
+     * o endereço invocado e o referer.
+     * @param request Objeto com as informações da requisição.
+     * @param incluirUri Flag indicando se a URI invocada deve constar nos detalhes.
+     * @param incluirReferer Flag indicando se o referer deve constar nos detalhes.
+     * @return <code>String</code>
+     */
+    public static synchronized String requestDetails(HttpServletRequest request, boolean incluirUri, boolean incluirReferer) {
+        StringBuilder sb = new StringBuilder("Requisicao [")
+            .append(request.getRemoteAddr()).append(", ");
+
+        String principal = null;
+        if (request.getUserPrincipal() != null)
+            principal = request.getUserPrincipal().getName();
+        else
+            principal = SpringSecurityToys.getPrincipalName();
+        if (principal != null)
+            sb.append("username=").append(principal).append(", ");
+
+        if (incluirUri)
+            sb.append("uri=").append(request.getRequestURI()).append(", ");
+
+        if (incluirReferer)
+            sb.append("referer=").append(request.getHeader("Referer")).append(", ");
+
+        sb.setLength(sb.length() - 2);
+        sb.append("]");
+
+        return sb.toString();
+    }
+
+    /**
      * Retorna uma mensagem padrão para logs de aplicações web.
      * @param request Requisição original
      * @param detalhes Se for especificado TRUE inclui URI e REFERER na texto gerado.
