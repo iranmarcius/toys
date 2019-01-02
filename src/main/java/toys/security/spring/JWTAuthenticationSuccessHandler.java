5c152ed94c3f2b08a35ed9fa96a93aa78819051d
Iran Marcius <iran@toledo.br>
2019-01-02 14:20:21 -0200

    Formatação de código.

--- src/main/java/toys/security/spring/JWTAuthenticationSuccessHandler.java
+++ src/main/java/toys/security/spring/JWTAuthenticationSuccessHandler.java
@@ -18,102 +18,104 @@ import java.util.Date;
 
 /**
  * Este filtro é responsável por gerar um JWT e retorná-lo através do cabeçalho <code>Authorization</code>.
+ *
  * @author Iran
  * @since 13/11/2018
  */
 public class JWTAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-    private RequestCache requestCache = new HttpSessionRequestCache();
-    private String issuer;
-    private long ttl;
-    private Key key;
+	private RequestCache requestCache = new HttpSessionRequestCache();
+	private String issuer;
+	private long ttl;
+	private Key key;
 
-    /**
-     * construtor padrão.
-     * @param issuer emissor do token.
-     * @param ttl Tempo de vida em milissegundos.
-     * @param key Chave utilizada na assinatura.
-     */
-    public JWTAuthenticationSuccessHandler(String issuer, long ttl, Key key) {
-        super();
-        this.issuer = issuer;
-        this.ttl = ttl;
-        this.key = key;
-    }
+	/**
+	 * construtor padrão.
+	 *
+	 * @param issuer emissor do token.
+	 * @param ttl    Tempo de vida em milissegundos.
+	 * @param key    Chave utilizada na assinatura.
+	 */
+	public JWTAuthenticationSuccessHandler(String issuer, long ttl, Key key) {
+		super();
+		this.issuer = issuer;
+		this.ttl = ttl;
+		this.key = key;
+	}
 
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        SavedRequest savedRequest = requestCache.getRequest(request, response);
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+		SavedRequest savedRequest = requestCache.getRequest(request, response);
 
-        String principal = SpringSecurityToys.getPrincipalName();
-        if (principal != null) {
-            logger.debug("Gerando token JWT para o principal autenticado.");
+		String principal = SpringSecurityToys.getPrincipalName();
+		if (principal != null) {
+			logger.debug("Gerando token JWT para o principal autenticado.");
 
-            JwtBuilder builder = Jwts.builder()
-                .setId(String.format("%d", System.currentTimeMillis()))
-                .setIssuedAt(new Date())
-                .setSubject(principal);
+			JwtBuilder builder = Jwts.builder()
+				.setId(String.format("%d", System.currentTimeMillis()))
+				.setIssuedAt(new Date())
+				.setSubject(principal);
 
-            if (StringUtils.isNotBlank(issuer)) {
-                logger.debug("Issuer: " + issuer);
-                builder.setIssuer(issuer);
-            }
+			if (StringUtils.isNotBlank(issuer)) {
+				logger.debug("Issuer: " + issuer);
+				builder.setIssuer(issuer);
+			}
 
-            if (ttl > 0) {
-                logger.debug("Tempo de vida em milissegundos: " + ttl);
-                builder.setExpiration(new Date(System.currentTimeMillis() + ttl));
-            }
+			if (ttl > 0) {
+				logger.debug("Tempo de vida em milissegundos: " + ttl);
+				builder.setExpiration(new Date(System.currentTimeMillis() + ttl));
+			}
 
-            if (key != null)
-                builder.signWith(SignatureAlgorithm.HS256, key);
+			if (key != null)
+				builder.signWith(SignatureAlgorithm.HS256, key);
 
-            String token = builder.compact();
-            logger.debug("Token gerado: " + token);
-            response.setHeader("Authorization", token);
-        } else {
-            logger.error("Nenhum nome de usuario encontrado no contexto para gerar o token.");
-            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Nenhum usuario autenticado no contexto.");
-        }
+			String token = builder.compact();
+			logger.debug("Token gerado: " + token);
+			response.setHeader("Authorization", token);
+		} else {
+			logger.error("Nenhum nome de usuario encontrado no contexto para gerar o token.");
+			response.sendError(HttpServletResponse.SC_FORBIDDEN, "Nenhum usuario autenticado no contexto.");
+		}
 
-        if (savedRequest == null) {
-            clearAuthenticationAttributes(request);
-            return;
-        }
+		if (savedRequest == null) {
+			clearAuthenticationAttributes(request);
+			return;
+		}
 
-        String targetUrlParam = getTargetUrlParameter();
-        if (isAlwaysUseDefaultTargetUrl() || (targetUrlParam != null && StringUtils.isNotBlank(request.getParameter(targetUrlParam)))) {
-            requestCache.removeRequest(request, response);
-            return;
-        }
+		String targetUrlParam = getTargetUrlParameter();
+		if (isAlwaysUseDefaultTargetUrl() || (targetUrlParam != null && StringUtils.isNotBlank(request.getParameter(targetUrlParam)))) {
+			requestCache.removeRequest(request, response);
+			return;
+		}
 
-        clearAuthenticationAttributes(request);
-    }
+		clearAuthenticationAttributes(request);
+	}
 
-    public void setRequestCache(RequestCache requestCache) {
-        this.requestCache = requestCache;
-    }
+	public void setRequestCache(RequestCache requestCache) {
+		this.requestCache = requestCache;
+	}
 
-    public String getIssuer() {
-        return issuer;
-    }
+	public String getIssuer() {
+		return issuer;
+	}
 
-    public void setIssuer(String issuer) {
-        this.issuer = issuer;
-    }
+	public void setIssuer(String issuer) {
+		this.issuer = issuer;
+	}
 
-    public long getTtl() {
-        return ttl;
-    }
+	public long getTtl() {
+		return ttl;
+	}
 
-    public void setTtl(long ttl) {
-        this.ttl = ttl;
-    }
+	public void setTtl(long ttl) {
+		this.ttl = ttl;
+	}
 
-    public Key getKey() {
-        return key;
-    }
+	public Key getKey() {
+		return key;
+	}
 
-    public void setKey(Key key) {
-        this.key = key;
-    }
+	public void setKey(Key key) {
+		this.key = key;
+	}
 
 }
