d27004ce690622bb78a7e96447c76d897cdc65eb
Iran Marcius <iran@unitoledo.br>
2020-04-09 13:30:01 -0300

    Criação de testes de autenticação em mais de um servidor LDAP.

--- toys/src/test/java/toys/tests/junit/LDAPTest.java
+++ toys/src/test/java/toys/tests/junit/LDAPTest.java
@@ -11,32 +11,44 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LDAPTest {
-    private static LDAPUtils ldapUtils;
-    private static final String USERNAME = "teste";
+    private static LDAPUtils ldapUtilsAcad;
+    private static LDAPUtils ldapUtilsCorp;
+    private static final String USERNAME = "testeacad";
     private static final String PASSWORD = "";
 
     @BeforeClass
     public static void inicializar() {
-        ldapUtils = new LDAPUtils(
+
+        ldapUtilsAcad = new LDAPUtils(
             "adele-2.unitoledo.acad",
             "Administrador@unitoledo.acad",
             "KLyz+ami1AsVCROZUhJjaw==",
             "DC=unitoledo,DC=acad",
             "(sAMAccountName=%s)"
         );
+
+        ldapUtilsCorp = new LDAPUtils(
+            "adnet.unitoledo.corp",
+            "Administrador@unitoledo.corp",
+            "KLyz+ami1AsVCROZUhJjaw==",
+            "DC=unitoledo,DC=corp",
+            "(sAMAccountName=%s)"
+        );
+
     }
 
     @Test
-    public void ldapAuthTest() throws LDAPException {
-        var entry = ldapUtils.pesquisar(USERNAME);
-        String erro = ldapUtils.autenticar(entry, PASSWORD);
+    public void testAuthAcad() throws LDAPException {
+        var entry = ldapUtilsAcad.pesquisar("teste-acad");
+        String erro = ldapUtilsAcad.autenticar(entry, "testeacad");
         assertNull(erro);
     }
 
     @Test
-    public void ldapChangePasswordTest() throws GeneralSecurityException, LDAPException {
-        ldapUtils.alterarSenha(USERNAME, PASSWORD, true);
-        assertTrue(Boolean.TRUE);
+    public void testAuthCorp() throws LDAPException {
+        var entry = ldapUtilsCorp.pesquisar("teste-corp");
+        String erro = ldapUtilsCorp.autenticar(entry, "testecorp");
+        assertNull(erro);
     }
 
 }
