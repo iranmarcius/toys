d79934b6715257d9b9ada40bdf8d6256e51e8111
Iran Marcius <iran@unitoledo.br>
2020-04-14 18:13:07 -0300

    Criação de exceptins especializadas.

---
+++ toys/src/main/java/toys/exceptions/ToysLDAPException.java
@@ -0,0 +1,39 @@
+package toys.exceptions;
+
+import toys.LDAPUtils;
+
+public class ToysLDAPException extends ToysException {
+    private static final long serialVersionUID = 1610066187127484595L;
+
+    public ToysLDAPException() {
+    }
+
+    public ToysLDAPException(String message) {
+        super(message);
+    }
+
+    public ToysLDAPException(Throwable cause) {
+        super(cause);
+    }
+
+    public ToysLDAPException(String message, Object... params) {
+        super(message, params);
+    }
+
+    public ToysLDAPException(String message, Throwable cause, Object... params) {
+        super(message, cause, params);
+    }
+
+    public ToysLDAPException(String message, LDAPUtils ldapUtils) {
+        this("%s %s", message, ldapUtils);
+    }
+
+    public ToysLDAPException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ToysLDAPException(String message, LDAPUtils ldapUtils, Throwable cause) {
+        this("%s %s", message, cause, ldapUtils);
+    }
+
+}
