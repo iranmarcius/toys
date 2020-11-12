cd174a4b1839961d281259607c27f4cc84af4704
Iran Marcius <iran.marcius@unitoledo.br>
2020-11-12 14:12:35 -0300

    Inclusão de propriedade.

--- toys/src/main/java/toys/application/ProgressMessage.java
+++ toys/src/main/java/toys/application/ProgressMessage.java
@@ -3,6 +3,7 @@ package toys.application;
 import toys.enums.Level;
 
 public class ProgressMessage {
+    private String pid;
     private int total;
     private int current;
     private String text;
@@ -11,27 +12,29 @@ public class ProgressMessage {
     public ProgressMessage() {
     }
 
-    public ProgressMessage(int total, int current, String text, Level level) {
+    public ProgressMessage(String pid, int total, int current, String text, Level level) {
+        this.pid = pid;
         this.total = total;
         this.current = current;
         this.text = text;
         this.level = level;
     }
 
-    public ProgressMessage(int total, int current, String text) {
-        this(total, current, text, null);
+    public ProgressMessage(String pid, int total, int current, String text) {
+        this(pid, total, current, text, null);
     }
 
-    public ProgressMessage(int total, int current) {
-        this(total, current, null);
+    public ProgressMessage(String pid, int total, int current) {
+        this(pid, total, current, null);
     }
 
-    public ProgressMessage(String text) {
-        this(0, 0, text, null);
+    public ProgressMessage(String pid, String text) {
+        this(pid, 0, 0, text, null);
     }
 
     public ProgressMessage(ProgressNotifier notifier) {
         this(
+            notifier.getPid(),
             notifier.getTotal(),
             notifier.getCurrent(),
             notifier.getText(),
@@ -41,8 +44,8 @@ public class ProgressMessage {
 
     @Override
     public String toString() {
-        return String.format("ProgressMessage [total=%s, current=%s, text=%s, level=%s]",
-            total, current, text, level);
+        return String.format("ProgressMessage [pid=%s, total=%s, current=%s, text=%s, level=%s]",
+            pid, total, current, text, level);
     }
 
     public int getTotal() {
@@ -63,6 +66,10 @@ public class ProgressMessage {
 
     public int getPercent() {
         return (int) (((double) current / (double) total) * 100);
+    }
+
+    public String getPid() {
+        return pid;
     }
 
 }
