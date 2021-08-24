6d15614e7fe98ff2d5f4c36c556fe9a79d470413
Iran Marcius <iran.marcius@unitoledo.br>
2021-08-24 11:39:46 -0300

    Criação de método utilitário para inserção de itens ordenadamente em uma lista.

--- toys/src/main/java/toys/CollectionToys.java
+++ toys/src/main/java/toys/CollectionToys.java
@@ -14,121 +14,136 @@ import java.util.*;
  */
 public class CollectionToys {
 
-    private CollectionToys() {
-    }
+  private CollectionToys() {
+  }
 
-    /**
-     * Fragmenta uma lista original em várias sublistas obedecendo o tamanho máximo especificado.
-     *
-     * @param l       Lista original.
-     * @param tamanho Tamanho máximo das sublistas.
-     * @return <code>List&lt;List&lt;?&gt;&gt;</code>
-     */
-    public static <T> List<List<T>> subLists(List<T> l, int tamanho) {
-        int i = 0;
-        int f;
-        List<List<T>> listas = new ArrayList<>();
-        while (i < l.size()) {
-            f = i + tamanho;
-            if (f > l.size())
-                f = l.size();
-            listas.add(l.subList(i, f));
-            i += tamanho;
-        }
-        return listas;
+  /**
+   * Fragmenta uma lista original em várias sublistas obedecendo o tamanho máximo especificado.
+   *
+   * @param l       Lista original.
+   * @param tamanho Tamanho máximo das sublistas.
+   * @return <code>List&lt;List&lt;?&gt;&gt;</code>
+   */
+  public static <T> List<List<T>> subLists(List<T> l, int tamanho) {
+    int i = 0;
+    int f;
+    List<List<T>> listas = new ArrayList<>();
+    while (i < l.size()) {
+      f = i + tamanho;
+      if (f > l.size())
+        f = l.size();
+      listas.add(l.subList(i, f));
+      i += tamanho;
     }
+    return listas;
+  }
 
-    /**
-     * Retorna uma lista convertida em string utilizando os separadores informados e
-     * envolvendo cada item com o quote informado.
-     *
-     * @param c     Lista de itens.
-     * @param sep   Separator de itens.
-     * @param quote Sinal para ser utilizado como quote.
-     * @return <code>String</code>
-     */
-    public static String asString(Collection<?> c, String sep, String quote) {
-        if (c == null || c.isEmpty())
-            return "";
-        StringBuilder sb = new StringBuilder();
-        for (Object o : c) {
-            if (quote != null)
-                sb.append(quote);
-            sb.append(o);
-            if (quote != null)
-                sb.append(quote);
-            if (sep != null)
-                sb.append(sep);
-        }
-        if (sep != null && sb.length() > sep.length())
-            sb.setLength(sb.length() - sep.length());
-        return sb.toString();
+  /**
+   * Retorna uma lista convertida em string utilizando os separadores informados e
+   * envolvendo cada item com o quote informado.
+   *
+   * @param c     Lista de itens.
+   * @param sep   Separator de itens.
+   * @param quote Sinal para ser utilizado como quote.
+   * @return <code>String</code>
+   */
+  public static String asString(Collection<?> c, String sep, String quote) {
+    if (c == null || c.isEmpty())
+      return "";
+    StringBuilder sb = new StringBuilder();
+    for (Object o : c) {
+      if (quote != null)
+        sb.append(quote);
+      sb.append(o);
+      if (quote != null)
+        sb.append(quote);
+      if (sep != null)
+        sb.append(sep);
     }
+    if (sep != null && sb.length() > sep.length())
+      sb.setLength(sb.length() - sep.length());
+    return sb.toString();
+  }
 
-    /**
-     * Retorna uma lista convertida em string utilizando o separador informado.
-     *
-     * @param l         Lista de itens.
-     * @param separator Separador.
-     * @return <code>String</code>
-     */
-    public static String asString(Collection<?> l, String separator) {
-        return asString(l, separator, null);
-    }
+  /**
+   * Retorna uma lista convertida em string utilizando o separador informado.
+   *
+   * @param l         Lista de itens.
+   * @param separator Separador.
+   * @return <code>String</code>
+   */
+  public static String asString(Collection<?> l, String separator) {
+    return asString(l, separator, null);
+  }
 
-    /**
-     * Retorna uma lista convertida em string com seus itens separados por vírgula.
-     *
-     * @param l Lista com os valores.
-     * @return <code>String</code>
-     */
-    public static String asString(Collection<?> l) {
-        return asString(l, ", ", null);
-    }
+  /**
+   * Retorna uma lista convertida em string com seus itens separados por vírgula.
+   *
+   * @param l Lista com os valores.
+   * @return <code>String</code>
+   */
+  public static String asString(Collection<?> l) {
+    return asString(l, ", ", null);
+  }
 
-    /**
-     * Converte uma lista com elementos duplicados para uma lista com elementos distintos.
-     *
-     * @param l Lista com elementos dupicados.
-     * @return Lista com elementos distintos.
-     */
-    public static <T> List<T> distinctList(List<T> l) {
-        List<T> result = new ArrayList<>(l.size());
-        Set<Identity> distinct = new HashSet<>();
-        for (T entity : l) {
-            if (distinct.add(new Identity(entity)))
-                result.add(entity);
-        }
-        return result;
+  /**
+   * Converte uma lista com elementos duplicados para uma lista com elementos distintos.
+   *
+   * @param l Lista com elementos dupicados.
+   * @return Lista com elementos distintos.
+   */
+  public static <T> List<T> distinctList(List<T> l) {
+    List<T> result = new ArrayList<>(l.size());
+    Set<Identity> distinct = new HashSet<>();
+    for (T entity : l) {
+      if (distinct.add(new Identity(entity)))
+        result.add(entity);
     }
+    return result;
+  }
 
-    /**
-     * Método de conveniência que utiliza o {@link Collections#binarySearch(List, Object, Comparator)} para localizar
-     * e retornar o elemento correspondente à chave informada dentro de uma lista. A lista deve estar previamente ordenada
-     * utilizando o mesmo comparator informado.
-     *
-     * @param key  Chave do objeto pesquisado.
-     * @param l    Lista a ser pesquisada. Deve estar classificada pelo mesmo comparator informado no parâmetro.
-     * @param comp comparator a ser utilizado na pesquisa.
-     * @return Retorna o objeto correspondente à chave ou null caso ele não seja encontrado.
-     */
-    public static <T> T find(T key, List<T> l, Comparator<T> comp) {
-        int i = Collections.binarySearch(l, key, comp);
-        return i >= 0 ? l.get(i) : null;
-    }
+  /**
+   * Método de conveniência que utiliza o {@link Collections#binarySearch(List, Object, Comparator)} para localizar
+   * e retornar o elemento correspondente à chave informada dentro de uma lista. A lista deve estar previamente ordenada
+   * utilizando o mesmo comparator informado.
+   *
+   * @param key  Chave do objeto pesquisado.
+   * @param l    Lista a ser pesquisada. Deve estar classificada pelo mesmo comparator informado no parâmetro.
+   * @param comp comparator a ser utilizado na pesquisa.
+   * @return Retorna o objeto correspondente à chave ou null caso ele não seja encontrado.
+   */
+  public static <T> T find(T key, List<T> l, Comparator<T> comp) {
+    int i = Collections.binarySearch(l, key, comp);
+    return i >= 0 ? l.get(i) : null;
+  }
 
-    /**
-     * Método de conveniência que utiliza o {@link Collections#binarySearch(List, Object)} para localizar e retornar o elemento
-     * correspondente à chave informada dentro de uma lista. A lista deve estar previamente ordenada.
-     *
-     * @param key Chave do objeto pesquisado.
-     * @param l   Lista a ser pesquisada. Deve estar classificada pelo mesmo comparator informado no parâmetro.
-     * @return Retorna o objeto correspondente à chave ou null caso ele não seja encontrado.
-     */
-    @SuppressWarnings("unchecked")
-    public static <T> T find(T key, List<T> l) {
-        int i = Collections.binarySearch((List<Comparable<T>>) l, key);
-        return i >= 0 ? l.get(i) : null;
-    }
+  /**
+   * Método de conveniência que utiliza o {@link Collections#binarySearch(List, Object)} para localizar e retornar o elemento
+   * correspondente à chave informada dentro de uma lista. A lista deve estar previamente ordenada.
+   *
+   * @param key Chave do objeto pesquisado.
+   * @param l   Lista a ser pesquisada. Deve estar classificada pelo mesmo comparator informado no parâmetro.
+   * @return Retorna o objeto correspondente à chave ou null caso ele não seja encontrado.
+   */
+  @SuppressWarnings("unchecked")
+  public static <T> T find(T key, List<T> l) {
+    int i = Collections.binarySearch((List<Comparable<T>>) l, key);
+    return i >= 0 ? l.get(i) : null;
+  }
+
+  /**
+   * Insere um valor em uma lista mantendo a classificação determinada pelo comparator fornecido.
+   *
+   * @param value      Valor a ser inserido na lista.
+   * @param list       Lista onde o valor será inserido.
+   * @param comparator função de comparação.
+   */
+  public static <T> void add(T value, List<T> list, Comparator<T> comparator) {
+    int i = Collections.binarySearch(list, value, comparator);
+    if (i < 0)
+      list.add(i * -1 - 1, value);
+    else
+      list.add(i, value);
+  }
 
 }
--- toys/src/test/java/toys/tests/junit/CollectionToysTest.java
+++ toys/src/test/java/toys/tests/junit/CollectionToysTest.java
@@ -4,32 +4,46 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import toys.CollectionToys;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CollectionToysTest {
-    private static List<Integer> lista;
+  private static List<Integer> lista;
 
-    @BeforeAll
-    public static void setUp() {
-        lista = Arrays.asList(0, 1, 2, 3, 4, 5);
-    }
+  @BeforeAll
+  public static void setUp() {
+    lista = Arrays.asList(0, 1, 2, 3, 4, 5);
+  }
 
-    @Test
-    void testFragmentar() {
-        List<List<Integer>> listas = CollectionToys.subLists(lista, 3);
-        assertEquals(2, listas.size());
-        assertEquals(3, listas.get(0).size());
-        assertEquals(3, listas.get(1).size());
-    }
+  @Test
+  void testFragmentar() {
+    List<List<Integer>> listas = CollectionToys.subLists(lista, 3);
+    assertEquals(2, listas.size());
+    assertEquals(3, listas.get(0).size());
+    assertEquals(3, listas.get(1).size());
+  }
 
-    @Test
-    void testToStringList() {
-        assertEquals("0, 1, 2, 3, 4, 5", CollectionToys.asString(lista));
-        assertEquals("0-1-2-3-4-5", CollectionToys.asString(lista, "-"));
-        assertEquals("'0'-'1'-'2'-'3'-'4'-'5'", CollectionToys.asString(lista, "-", "'"));
-    }
+  @Test
+  void testToStringList() {
+    assertEquals("0, 1, 2, 3, 4, 5", CollectionToys.asString(lista));
+    assertEquals("0-1-2-3-4-5", CollectionToys.asString(lista, "-"));
+    assertEquals("'0'-'1'-'2'-'3'-'4'-'5'", CollectionToys.asString(lista, "-", "'"));
+  }
+
+  @Test
+  void testAdd() {
+    var l = new ArrayList<Integer>();
+    Comparator<Integer> comp = Comparator.comparingInt(v -> v);
+    CollectionToys.add(7, l, comp);
+    CollectionToys.add(5, l, comp);
+    CollectionToys.add(10, l, comp);
+    CollectionToys.add(6, l, comp);
+    CollectionToys.add(6, l, comp);
+    assertEquals(Arrays.asList(5, 6, 6, 7, 10), l);
+  }
 
 }
