f95880103add021bdf96d5aa945501dcb01e2aca
Iran Marcius <iran.marcius@unitoledo.br>
2021-09-20 18:53:50 -0300

    Correção de escopo de métodos e reformatação do código.

--- toys/src/main/java/toys/DirCrawler.java
+++ toys/src/main/java/toys/DirCrawler.java
@@ -15,67 +15,66 @@ import java.io.FileFilter;
  * @author Iran
  */
 public abstract class DirCrawler {
-    protected String rootDir;
-    private FileFilter directoryFilter;
+  protected String rootDir;
+  private final FileFilter directoryFilter;
 
-    /**
-     * Construtor padrão.
-     */
-    public DirCrawler() {
-        super();
-        directoryFilter = File::isDirectory;
+  /**
+   * Construtor padrão.
+   */
+  protected DirCrawler() {
+    directoryFilter = File::isDirectory;
+  }
+
+  /**
+   * Construtor com inicialização de propriedades.
+   *
+   * @param rootDir Diretório inicial.
+   */
+  protected DirCrawler(String rootDir) {
+    this();
+    this.rootDir = rootDir;
+  }
+
+  /**
+   * Navega pelos diretórios a partir do diretório raiz.
+   */
+  protected void crawl() throws ToysException {
+    crawl(rootDir);
+  }
+
+  /**
+   * Seta o diretório raiz e navega todos os subdiretórios a partir dele.
+   *
+   * @param rootDir Caminho do diretório raiz para a operação.
+   */
+  protected void crawl(String rootDir) throws ToysException {
+    this.rootDir = rootDir;
+    recurseProcess(new File(rootDir), 0);
+  }
+
+  /**
+   * Processa o diretório e todos os seus subdiretórios.
+   *
+   * @param dir   Referência para o diretório inicial.
+   * @param level Nível do diretório que está sendo processado.
+   */
+  private void recurseProcess(File dir, int level) throws ToysException {
+    if (dir.isDirectory()) {
+      process(dir, level);
+      File[] subDirs = dir.listFiles(directoryFilter);
+      if (subDirs != null) {
+        for (File d : subDirs)
+          recurseProcess(d, level + 1);
+      }
     }
+  }
 
-    /**
-     * Construtor com inicialização de propriedades.
-     *
-     * @param rootDir Diretório inicial.
-     */
-    public DirCrawler(String rootDir) {
-        this();
-        this.rootDir = rootDir;
-    }
-
-    /**
-     * Navega pelos diretórios a partir do diretório raiz.
-     */
-    protected void crawl() throws ToysException {
-        crawl(rootDir);
-    }
-
-    /**
-     * Seta o diretório raiz e navega todos os subdiretórios a partir dele.
-     *
-     * @param rootDir Caminho do diretório raiz para a operação.
-     */
-    protected void crawl(String rootDir) throws ToysException {
-        this.rootDir = rootDir;
-        recurseProcess(new File(rootDir), 0);
-    }
-
-    /**
-     * Processa o diretório e todos os seus subdiretórios.
-     *
-     * @param dir   Referência para o diretório inicial.
-     * @param level Nível do diretório que está sendo processado.
-     */
-    private void recurseProcess(File dir, int level) throws ToysException {
-        if (dir.isDirectory()) {
-            process(dir, level);
-            File[] subDirs = dir.listFiles(directoryFilter);
-            if (subDirs != null) {
-                for (File d : subDirs)
-                    recurseProcess(d, level + 1);
-            }
-        }
-    }
-
-    /**
-     * Método para o processamento do diretório.
-     *
-     * @param dir   Referência para o diretório a ser processado.
-     * @param level Nível de profundidade do diretório que está sendo processado.
-     */
-    protected abstract void process(File dir, int level) throws ToysException;
+  /**
+   * Método para o processamento do diretório.
+   *
+   * @param dir   Referência para o diretório a ser processado.
+   * @param level Nível de profundidade do diretório que está sendo processado.
+   */
+  protected abstract void process(File dir, int level) throws ToysException;
 
 }
