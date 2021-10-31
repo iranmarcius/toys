dedc4bbf0b9333e2337de2956e84791619a560b6
Iran Marcius <iran.marcius@unitoledo.br>
2021-10-31 11:12:14 -0300

    Alteração na forma como a string de detalhes de requisição é construída.

--- toys-servlet/src/main/java/toys/servlet/RequestDetailsBuilder.java
+++ toys-servlet/src/main/java/toys/servlet/RequestDetailsBuilder.java
@@ -15,97 +15,105 @@ import static toys.ToysConsts.*;
  * @since 10/2020
  */
 public class RequestDetailsBuilder {
-    private final HttpServletRequest request;
-    private String principal;
-    private String userAgent;
-    private String referer;
-    private String xForwardedFor;
-    private Map<String, Object> params;
+  private final HttpServletRequest request;
+  private String principal;
+  private String uri;
+  private String userAgent;
+  private String referer;
+  private String xForwardedFor;
+  private Map<String, Object> params;
 
-    public RequestDetailsBuilder(HttpServletRequest request) {
-        this.request = request;
-    }
+  public RequestDetailsBuilder(HttpServletRequest request) {
+    this.request = request;
+  }
 
-    public RequestDetailsBuilder withPrincipal(String principalName) {
-        principal = principalName;
-        return this;
-    }
+  public RequestDetailsBuilder withPrincipal(String principalName) {
+    principal = principalName;
+    return this;
+  }
 
-    public RequestDetailsBuilder withPrincipal() {
-        var userPrincipal = request.getUserPrincipal();
-        if (userPrincipal != null)
-            return withPrincipal(userPrincipal.getName());
-        else
-            return this;
-    }
+  public RequestDetailsBuilder withURI() {
+    uri = request.getRequestURI();
+    return this;
+  }
 
-    public RequestDetailsBuilder withUserAgent() {
-        userAgent = request.getHeader(HTTP_HEADER_USER_AGENT);
-        return this;
-    }
+  public RequestDetailsBuilder withPrincipal() {
+    var userPrincipal = request.getUserPrincipal();
+    if (userPrincipal != null)
+      return withPrincipal(userPrincipal.getName());
+    else
+      return this;
+  }
 
-    public RequestDetailsBuilder withReferer() {
-        referer = request.getHeader(HTTP_HEADER_REFERER);
-        return this;
-    }
+  public RequestDetailsBuilder withUserAgent() {
+    userAgent = request.getHeader(HTTP_HEADER_USER_AGENT);
+    return this;
+  }
 
-    public RequestDetailsBuilder withXForwardedFor() {
-        xForwardedFor = request.getHeader(HTTP_HEADER_X_FORWARDED_FOR);
-        return this;
-    }
+  public RequestDetailsBuilder withReferer() {
+    referer = request.getHeader(HTTP_HEADER_REFERER);
+    return this;
+  }
 
-    public RequestDetailsBuilder withRequestParam(String paramName) {
-        if (params == null)
-            params = new HashMap<>();
-        params.put(paramName, request.getParameter(paramName));
-        return this;
-    }
+  public RequestDetailsBuilder withXForwardedFor() {
+    xForwardedFor = request.getHeader(HTTP_HEADER_X_FORWARDED_FOR);
+    return this;
+  }
 
-    public RequestDetailsBuilder withAll(String principalName) {
-        return withPrincipal(principalName)
-            .withReferer()
-            .withUserAgent()
-            .withXForwardedFor();
-    }
+  public RequestDetailsBuilder withRequestParam(String paramName) {
+    if (params == null)
+      params = new HashMap<>();
+    params.put(paramName, request.getParameter(paramName));
+    return this;
+  }
 
-    public RequestDetailsBuilder withAll() {
-        return withPrincipal()
-            .withReferer()
-            .withUserAgent()
-            .withXForwardedFor();
-    }
+  public RequestDetailsBuilder withAll(String principalName) {
+    return withPrincipal(principalName)
+      .withURI()
+      .withReferer()
+      .withUserAgent()
+      .withXForwardedFor();
+  }
 
-    public String build() {
-        var sb = new StringBuilder("Requisicao [");
-        appendDetail(sb, "principal", principal);
-        appendParams(sb);
-        appendDetail(sb, "remoteAddress", request.getRemoteAddr());
-        appendDetail(sb, "requestUri", request.getRequestURI());
-        appendDetail(sb, "referer", referer);
-        appendDetail(sb, "xForwardedFor", xForwardedFor);
-        appendDetail(sb, "userAgent", userAgent);
-        sb.setLength(sb.length() - 2);
-        sb.append("]");
-        return sb.toString();
-    }
+  public RequestDetailsBuilder withAll() {
+    return withPrincipal()
+      .withURI()
+      .withReferer()
+      .withUserAgent()
+      .withXForwardedFor();
+  }
 
-    private void appendDetail(StringBuilder sb, String name, String value) {
-        if (StringUtils.isNotBlank(value))
-            sb.append(name).append("=").append(value).append(", ");
-    }
+  public String build() {
+    var sb = new StringBuilder("Requisicao [");
+    appendDetail(sb, "principal", principal);
+    appendParams(sb);
+    appendDetail(sb, "remoteAddress", request.getRemoteAddr());
+    appendDetail(sb, "uri", uri);
+    appendDetail(sb, "referer", referer);
+    appendDetail(sb, "xForwardedFor", xForwardedFor);
+    appendDetail(sb, "userAgent", userAgent);
+    sb.setLength(sb.length() - 2);
+    sb.append("]");
+    return sb.toString();
+  }
 
-    private void appendParams(StringBuilder sb) {
-        if (params == null || params.isEmpty())
-            return;
-        sb.append("params={");
-        for (Map.Entry<String, Object> entry: params.entrySet())
-            sb.append(entry.getKey()).append("=").append(entry.getValue()).append(", ");
-        sb.setLength(sb.length() - 2);
-        sb.append("}, ");
-    }
+  private void appendDetail(StringBuilder sb, String name, String value) {
+    if (StringUtils.isNotBlank(value))
+      sb.append(name).append("=").append(value).append(", ");
+  }
 
-    public static RequestDetailsBuilder builder(HttpServletRequest request) {
-        return new RequestDetailsBuilder(request);
-    }
+  private void appendParams(StringBuilder sb) {
+    if (params == null || params.isEmpty())
+      return;
+    sb.append("params={");
+    for (Map.Entry<String, Object> entry : params.entrySet())
+      sb.append(entry.getKey()).append("=").append(entry.getValue()).append(", ");
+    sb.setLength(sb.length() - 2);
+    sb.append("}, ");
+  }
+
+  public static RequestDetailsBuilder builder(HttpServletRequest request) {
+    return new RequestDetailsBuilder(request);
+  }
 
 }
