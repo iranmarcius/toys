078949cfec9be8118ed6adfc2cd425e8ebccc103
Iran Marcius <iran.marcius@unitoledo.br>
2022-03-01 11:34:45 -0300

    Criação de tipo de erro e atualização de versões de módulos.

---
+++ toys/src/main/java/toys/exceptions/ToysMessagesRuntimeException.java
@@ -0,0 +1,49 @@
+package toys.exceptions;
+
+import toys.pojos.TextMessagePojo;
+
+import java.io.Serial;
+import java.util.List;
+
+public class ToysMessagesRuntimeException extends ToysRuntimeException {
+
+  @Serial
+  private static final long serialVersionUID = 6886280887692087341L;
+
+  private final transient List<TextMessagePojo> messages;
+
+  public ToysMessagesRuntimeException(List<TextMessagePojo> messages) {
+    super();
+    this.messages = messages;
+  }
+
+  public ToysMessagesRuntimeException(String message, List<TextMessagePojo> messages) {
+    super(message);
+    this.messages = messages;
+  }
+
+  public ToysMessagesRuntimeException(String message, List<TextMessagePojo> messages, Throwable cause) {
+    super(message, cause);
+    this.messages = messages;
+  }
+
+  public ToysMessagesRuntimeException(List<TextMessagePojo> messages, Throwable cause) {
+    super(cause);
+    this.messages = messages;
+  }
+
+  public ToysMessagesRuntimeException(String message, List<TextMessagePojo> messages, Object... params) {
+    super(message, params);
+    this.messages = messages;
+  }
+
+  public ToysMessagesRuntimeException(String message, List<TextMessagePojo> messages, Throwable cause, Object... params) {
+    super(message, cause, params);
+    this.messages = messages;
+  }
+
+  public List<TextMessagePojo> getMessages() {
+    return messages;
+  }
+
+}
