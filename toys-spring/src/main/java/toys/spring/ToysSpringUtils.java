794f576e079ceae0b75f913887b8c2f90f4c9ed1
Iran Marcius <iran.marcius@unitoledo.br>
2022-05-05 08:10:48 -0300

    Organização de imports e correção de erro de digitação na documentação.

--- toys-spring/src/main/java/toys/spring/ToysSpringUtils.java
+++ toys-spring/src/main/java/toys/spring/ToysSpringUtils.java
@@ -2,16 +2,9 @@ package toys.spring;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -28,7 +21,7 @@ public class ToysSpringUtils {
   /**
    * Retorna uma relação das autoridades à partir da autenticação informada.
    *
-   * @param authentication Objeto com os dados da autnticação.
+   * @param authentication Objeto com os dados da autenticação.
    * @return <code>List&lt;String&gt;</code>
    */
   public static Set<String> getAuthorities(Authentication authentication) {
