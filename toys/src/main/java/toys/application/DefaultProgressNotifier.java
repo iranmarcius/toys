c95647a94bab4f240690f99ede11c137e109c882
Iran Marcius <iran.marcius@unitoledo.br>
2022-08-15 14:18:01 -0300

    Criação de construtor default para o notificador de progresso padrão.

--- toys/src/main/java/toys/application/DefaultProgressNotifier.java
+++ toys/src/main/java/toys/application/DefaultProgressNotifier.java
@@ -9,105 +9,108 @@ import toys.enums.Level;
  * @since 02/2012
  */
 public class DefaultProgressNotifier implements ProgressNotifier {
-    private final String pid;
-    private int total;
-    private int current;
-    private String text;
-    private Level level;
-    private boolean cancelled;
+  private String pid;
+  private int total;
+  private int current;
+  private String text;
+  private Level level;
+  private boolean cancelled;
 
-    public DefaultProgressNotifier(String pid, int total) {
-        this.pid = pid;
-        this.total = total;
-    }
+  public DefaultProgressNotifier() {
+  }
 
-    public DefaultProgressNotifier(int total) {
-        this(null, total);
-    }
+  public DefaultProgressNotifier(String pid, int total) {
+    this.pid = pid;
+    this.total = total;
+  }
 
-    @Override
-    public void start(int total, String text, Object... params) {
-        this.total = total;
-        start(text, params);
-    }
+  public DefaultProgressNotifier(int total) {
+    this(null, total);
+  }
 
-    @Override
-    public void start(String text, Object... params) {
-        current = 0;
-        text(text, params);
-    }
+  @Override
+  public void start(int total, String text, Object... params) {
+    this.total = total;
+    start(text, params);
+  }
 
-    @Override
-    public void start(int total) {
-        start(total, null);
-    }
+  @Override
+  public void start(String text, Object... params) {
+    current = 0;
+    text(text, params);
+  }
 
-    @Override
-    public void step(int amount, String text, Level level, Object... params) {
-        current += amount;
-        text(text, level, params);
-    }
+  @Override
+  public void start(int total) {
+    start(total, null);
+  }
 
-    @Override
-    public void step(int amount, String text, Object... params) {
-        step(amount, text, null, params);
-    }
+  @Override
+  public void step(int amount, String text, Level level, Object... params) {
+    current += amount;
+    text(text, level, params);
+  }
 
-    @Override
-    public void text(String text, Level level, Object... params) {
-        this.text = params != null && params.length > 0 ? String.format(text, params) : text;
-        this.level = level;
-        onEvent(this);
-    }
+  @Override
+  public void step(int amount, String text, Object... params) {
+    step(amount, text, null, params);
+  }
 
-    @Override
-    public void text(String text, Object... params) {
-        text(text, null, params);
-    }
+  @Override
+  public void text(String text, Level level, Object... params) {
+    this.text = params != null && params.length > 0 ? String.format(text, params) : text;
+    this.level = level;
+    onEvent(this);
+  }
 
-    @Override
-    public void cancel() {
-        cancelled = true;
-    }
+  @Override
+  public void text(String text, Object... params) {
+    text(text, null, params);
+  }
 
-    @Override
-    public boolean isCancelled() {
-        return cancelled;
-    }
+  @Override
+  public void cancel() {
+    cancelled = true;
+  }
 
-    @Override
-    public int getPercent() {
-        return (int) (((double) current / (double) total) * 100);
-    }
+  @Override
+  public boolean isCancelled() {
+    return cancelled;
+  }
 
-    @Override
-    public int getTotal() {
-        return total;
-    }
+  @Override
+  public int getPercent() {
+    return (int) (((double) current / (double) total) * 100);
+  }
 
-    @Override
-    public int getCurrent() {
-        return current;
-    }
+  @Override
+  public int getTotal() {
+    return total;
+  }
 
-    @Override
-    public String getText() {
-        return text;
-    }
+  @Override
+  public int getCurrent() {
+    return current;
+  }
 
-    @Override
-    public Level getLevel() {
-        return level;
-    }
+  @Override
+  public String getText() {
+    return text;
+  }
 
-    @Override
-    public String getPid() {
-        return pid;
-    }
+  @Override
+  public Level getLevel() {
+    return level;
+  }
 
-    @Override
-    public void onEvent(ProgressNotifier notifier) {
-        // Nada ocorre aqui
-    }
+  @Override
+  public String getPid() {
+    return pid;
+  }
+
+  @Override
+  public void onEvent(ProgressNotifier notifier) {
+    // Nada ocorre aqui
+  }
 
 }
