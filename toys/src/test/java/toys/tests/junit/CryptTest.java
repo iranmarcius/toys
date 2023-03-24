dbd78fad02624e05769ba73b736302296187f024
Iran Marcius <iranmarcius@hotmail.com>
2023-03-24 16:26:56 -0300

    Atualização do teste devido à exclusão de classe.

--- toys/src/test/java/toys/tests/junit/CryptTest.java
+++ toys/src/test/java/toys/tests/junit/CryptTest.java
@@ -2,10 +2,10 @@ package toys.tests.junit;
 
 import org.junit.jupiter.api.Test;
 import toys.Crypt;
-import toys.ToysSecretKey;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -17,12 +17,18 @@ class CryptTest {
 
   @Test
   void testEncryptDecrypt() throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException {
+
+    // Gera uma chave
+    var kg = KeyGenerator.getInstance("DES");
+    kg.init(56);
+    var key = kg.generateKey();
+
+    // Realiza os testes de codificação.
     var s = "Iran Marcius";
-    var encoded = Base64.getUrlEncoder().encodeToString(Crypt.encrypt(s, ToysSecretKey.getInstance()));
+    var encoded = Base64.getUrlEncoder().encodeToString(Crypt.encrypt(s, key));
     System.out.println(encoded);
-    var decoded = Crypt.decrypt(Base64.getUrlDecoder().decode(encoded), ToysSecretKey.getInstance());
+    var decoded = Crypt.decrypt(Base64.getUrlDecoder().decode(encoded), key);
     assertEquals(s, decoded);
   }
 
 }
-
